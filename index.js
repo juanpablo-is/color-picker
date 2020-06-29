bf24ea8252e1197cf46754e83b22e86a063fad9d
Juan Barrera <juanpablo.is@outlook.com>
2020-06-28 21:35:45 -0500

    Mouse move test color

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let colorsPicker = document.getElementsByClassName('card-color');
 let alert = document.getElementById('alert');
-var clipboard = new Clipboard('.card-color');
+let clipboard = new Clipboard('.card-color');
+let colorDefault = '#e8ecf3';
+
+execPicker();
 
 document.addEventListener('keydown', (event) => {
     if (event.keyCode === 32 || event.keyCode === 13) {
@@ -8,19 +11,27 @@ document.addEventListener('keydown', (event) => {
     }
 });
 
-execPicker();
-
-
 function execPicker() {
     for (let i = 0; i < colorsPicker.length; i++) {
         let valorHTML = generateHTML();
         colorsPicker[i].setAttribute('data-clipboard-text', valorHTML);
-        
+
         let itemsColors = colorsPicker[i].children;
         itemsColors[0].style.background = valorHTML;
         itemsColors[1].innerHTML = valorHTML;
 
-        colorsPicker[i].addEventListener('click', function () {
+        colorsPicker[i].addEventListener('mousemove', function (e) {
+            if (e.ctrlKey) {
+                document.body.style.background = valorHTML;
+            }
+        });
+
+        colorsPicker[i].addEventListener('mouseleave', function (e) {
+            document.body.style.background = colorDefault;
+
+        });
+
+        colorsPicker[i].addEventListener('click', function (e) {
             alert.style.display = 'flex';
             document.querySelector('#alert span').innerHTML = valorHTML;
             setTimeout(function () { alert.style.display = 'none'; }, 2000);
