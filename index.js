f32ef4ce92e38b6f7744bfad4c662554088ca896
Juan Barrera <juanpablo.is@outlook.com>
2020-06-29 20:30:05 -0500

    Add color format function

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let colorsPicker = document.getElementsByClassName('card-color');
 let alert = document.getElementById('alert');
 let clipboard = new Clipboard('.card-color');
 let colorDefault = '#e8ecf3';
+let formatColor = 'hex';
+let alertFormatColor = document.getElementById('popup-format');
+let hexColor = [];
 
 execPicker();
 
@@ -11,9 +14,13 @@ document.addEventListener('keydown', (event) => {
     }
 });
 
+document.getElementById('popup-format-exit').addEventListener('click', () => {
+});
+
 function execPicker() {
     for (let i = 0; i < colorsPicker.length; i++) {
         let valorHTML = generateHTML();
+        hexColor[i] = valorHTML;
         colorsPicker[i].setAttribute('data-clipboard-text', valorHTML);
 
         let itemsColors = colorsPicker[i].children;
@@ -37,6 +44,9 @@ function execPicker() {
             setTimeout(function () { alert.style.display = 'none'; }, 2000);
         });
     }
+    if (formatColor !== 'hex') {
+        changeFormat(formatColor);
+    }
 }
 
 function generateHTML() {
@@ -47,4 +57,37 @@ function generateHTML() {
     }
 
     return html.toUpperCase();
+}
+
+function popupFormat(proceso) {
+    alertFormatColor.style.display = proceso ? "flex" : "none";
+    if (!proceso) {
+        changeFormat(formatColor = document.querySelector('input[name="format"]:checked').value);
+    }
+}
+
+function changeFormat(format) {
+    switch (format.toLowerCase()) {
+        case 'hex':
+            for (let i = 0; i < colorsPicker.length; i++) {
+                colorsPicker[i].lastElementChild.innerHTML = hexColor[i];
+                colorsPicker[i].setAttribute('data-clipboard-text', hexColor[i]);
+            }
+            break;
+        case 'rgb':
+            for (let i = 0; i < colorsPicker.length; i++) {
+                let rgb = "rgb("
+                let hexCard = hexColor[i].split('#')[1];
+                hexCard = hexCard.split('');
+                for (let j = 0; j < 6; j += 2) {
+                    rgb += (isNaN(hexCard[j]) ? hexCard[j].charCodeAt(0) - 55 : parseInt(hexCard[j])) * 16 + (isNaN(hexCard[j + 1]) ? hexCard[j + 1].charCodeAt(0) - 55 : parseInt(hexCard[j + 1]))
+                    rgb += ',';
+
+                }
+                rgb = rgb.substring(0, rgb.length - 1) + ")";
+                colorsPicker[i].lastElementChild.innerHTML = rgb;
+                colorsPicker[i].setAttribute('data-clipboard-text', rgb);
+            }
+            break;
+    }
 }
